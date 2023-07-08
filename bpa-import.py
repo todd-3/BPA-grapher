296aaaf59ff4e7401c037e3d5214fbe0a1fc7682
Roman T <104324796+todd-3@users.noreply.github.com>
2023-07-07 17:29:56 -0700

    Script can now either use a file on disk as its data source or pull an up to date documnet from internet
    - added ability to choose between file or web as data source
    - changed to custom parser for CSV parsing
    - added "total generation" and "excess" plots to graph

--- bpa-import.py
+++ bpa-import.py
@@ -1,37 +1,60 @@
 """
 Potential TODO ideas
-- plot graph directly from dataframe
+- create "Total Generated" column by summing all columns for given index except load
+- create inverse "Generated Overhead" column by graphing [...]
 """
 
 # imports
 from csv import DictReader
+import requests as req
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 from datetime import datetime as dates
 from time import strftime, localtime
+import CSV_string_parse as parser
+import argparse
+from pathlib import Path
 
+__version__ = 2.0
 
-# read_file = "BPA-data.txt"
-read_file = "BPA-09-21.txt"
+USE_WEB = True  # set True to load real-time data from web source
+file_name: Path = Path("test.txt")  # path to file to use if USE_WEB is false
+bpa_page = "https://transmission.bpa.gov/Business/Operations/Wind/baltwg.txt"  # URL to use if USE_WEB is true
+
 
 # function takes in datestamp in the form of "mm/dd/yyyy hh:mm" and returns converted unix timecode
 def to_unix(date) -> int:
-    # the formate of timestamps in the raw BPA data (written using pythons strptime notation)
-    bpa_format = "%m/%d/%Y %H:%M"
-
-    # converts passed datestamp to datetime object and derives unix timecode
-    return int(dates.strptime(date, bpa_format).timestamp())
+    bpa_format = "%m/%d/%Y %H:%M"  # the formate of timestamps in the raw BPA data (written using pythons strptime notation)
+    return int(dates.strptime(date, bpa_format).timestamp())  # converts passed datestamp to datetime object and derives unix timecode
 
 
-# ------------------------------------------------------------ #
-# Imports data from file and processes for graphing
+# URL document and file document have different newline delineators, if statement chooses appropriate one to use
+page_newline: str = ""
+page_raw: str = ""  # raw loaded document is saved here
+if USE_WEB:
+    print(f"Attempting to request document \'{bpa_page}\'")
+    page_newline = "\r\n"
 
-# Loads raw data from txt file using csv.DictReader and dumps to pandas dataframe
-with open(read_file, "r") as file_data:
-    read_data = DictReader(file_data, delimiter="\t")  # loads file using csv module with seperator set to tabs
-    load_data = DataFrame(read_data)
+    try:
+        page_raw = req.get(bpa_page).text  # load url using requests library
+    except req.exceptions.ConnectionError:  # catches exception if library cannot connect to url
+        print(f"could not connect to page \'{bpa_page}\'\n\tCheck if device is connected to internet")
+        exit()
+else:
+    print(f"loading file \'{file_name}\'")
+    page_newline = "\n"
+
+    with file_name.open() as file:  # pens file using pathlib.Path builtin
+        page_raw = file.read()  # grabs entire contents of file and loads as string
+
+page_process: list = page_raw.split(page_newline)[11:]  # removes first 11 lines of document (unneeded filler)
+page_build: str = "\n".join(page_process)  # rejoins document into string
+
+data = parser.row_list(page_build, "\t")  # calls parsing function to convert csv document to dict
+load_data: DataFrame = DataFrame(data)  # creates data frame with loaded csv data
 
 # removes all entries from dataframe that do not have a value in their "Load" field
+# BPA builds data table in such a way that often leads to empty rows (other than date string)
 blank_entries = load_data[load_data["Load"] == ''].index.values.tolist()
 load_data.drop(index=blank_entries, inplace=True)
 
@@ -47,16 +70,18 @@ load_data.set_index("Date", drop=True, inplace=True)
 # applies int() operator to all entries in dataframe
 load_data = load_data.applymap(lambda value: int(value))
 
-info_frame = load_data.aggregate(["idxmax", "idxmin"])
+load_data["Total Generation"] = load_data["VER"] + load_data["Hydro"] + load_data["Fossil/Biomass"] + load_data["Nuclear"]
+load_data["Excess"] = load_data["Total Generation"] - load_data["Load"]
+
+info_frame = load_data.aggregate(["max", "min", "average"])
+info_frame = info_frame.applymap(lambda value: int(value))
 print(info_frame)
-# exit()
 
 # ------------------------------------------------------------ #
 # plots bpa data to graph and modifies scaling and axis ticks to make chart usable
 
 # defines title and axis names of graph
 plt.title("BPA load graph")
-plt.xlabel("Time")
 plt.ylabel("Megawatts")
 
 
@@ -65,7 +90,7 @@ columns = load_data.columns.tolist()
 
 # iterates through all columns in dataframe and plots them to graph
 # column values are plotted to the y-axis and time is used for x
-colors = {"Load": "Red", "VER": "green", "Hydro": "blue", "Fossil/Biomass": "maroon", "Nuclear": "purple", "Total Generation": "gray"}  # colors match those from original BPA chart
+colors = {"Load": "Red", "VER": "green", "Hydro": "blue", "Fossil/Biomass": "maroon", "Nuclear": "purple", "Total Generation": "gray", "Excess": "pink"}  # colors match those from original BPA chart
 for column in columns:
     plt.plot(load_data.index.to_list(), load_data[column], label=column, color=colors[column])
 
@@ -89,10 +114,12 @@ plt.yticks(y_ticks, [str(val) for val in y_ticks])
 # visual modifiers
 plt.minorticks_on()
 plt.grid()
-plt.ylim(0, 11000)
+plt.ylim(0, 13000)
 plt.xlim(major_ticks[0], major_ticks[-1])
 plt.legend()
 plt.yscale("linear")
 
-plt.show()
+# TODO: Change background color to something less eye burning
 
+plt.savefig("test.jpg", dpi=300)
+# plt.show()
